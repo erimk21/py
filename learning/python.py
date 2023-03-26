ba35538e169fce23274c19bc40f513fdb345bb0a
erimk21 <9019259@student.zadkine.nl>
2023-03-26 22:13:41 +0200

    opzet 1

---
+++ learning/python.py
@@ -0,0 +1,33 @@
+#my first Python quiz
+
+# Vraag 1
+print("Vraag 1: Wat is de hoofdstad van Nederland?")
+antwoord_1 = input()
+
+if antwoord_1.lower() == "amsterdam":
+    print("Goed gedaan!")
+else:
+    print("Helaas, dat is niet juist.")
+
+# Vraag 2
+print("Vraag 2: Wat is de hoogste berg ter wereld?")
+antwoord_2 = input()
+
+if antwoord_2.lower() == "mount everest":
+    print("Goed gedaan!")
+else:
+    print("Helaas, dat is niet juist.")
+
+# Vraag 3
+print("Vraag 3: Wie was de eerste mens op de maan?")
+antwoord_3 = input()
+
+if antwoord_3.lower() == "neil armstrong":
+    print("Goed gedaan!")
+else:
+    print("Helaas, dat is niet juist.")
+
+# Eindresultaat
+print("Bedankt voor het spelen van de quiz!")
+
+
